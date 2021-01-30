6bd615e078dcb5ee5ca76f79b2a936d98bef6a0a
alegr <alegr@DESKTOP-7PMKL69>
2021-01-30 19:01:59 +0900

    commit

--- src/Chapter0.java
+++ src/Chapter0.java
@@ -1,7 +1,10 @@
 
 public class Chapter0 {
 	public static void main(String[] args) {
+		//コード0-1 HelloWorldプログラム
 		System.out.println("Hello World");
+		//コード0-2 一部を書き換えたプログラム
+		System.out.println("すがわら");
 	}
 
 
